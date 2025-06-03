e425a6f8bbcd0eec353bb81d05e0dba9dfe3d210
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-03 22:18:40 +0530

    fix(auth): resolved 'c is not defined' error by correcting middleware parameter

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,20 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-    
-  return c.json({
+async  function authMiddleware (c:any, next:any){
+  if(c.req.header("Authorization")){
+    await next();
+  }else{
+    return c.text("you dont have a access");
+  }
+}
 
-    message:"hii there"
-  })
+
+
+// app.use(authMiddleware);
+
+
+
+
+app.post('/', authMiddleware, async(c) => {
+    const body = await c.req.json();
+    console.log(body);
+    console.log(c.req.header("Authorization"));
+  console.log(c.req.query('param')); 
+  return c.text('Hello Hono!');
 })
 
 //  ha batao kay kaam hai 
 // aao khana khalo 
 // aaldy bahut kuch ho gaya
 
-app.post('/user', (c) => {
-  return c.text('Hello bro !')
-})
+// app.post('/user', (c) => {
+//   return c.text('Hello bro !')
+// })
 
 export default app
